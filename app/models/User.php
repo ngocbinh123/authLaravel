731833651af7bd3e967c532f985f1022a8100d89
nguyen ngoc binh <nguyengocbinh@gmail.com>
2014-09-02 22:43:49 -0700

    update user.php composer and add cashier_column

--- app/models/User.php
+++ app/models/User.php
@@ -4,10 +4,12 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Laravel\Cashier\BillableTrait;
+use Laravel\Cashier\BillableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface, BillableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait,BillableTrait;
 
 	/**
 	 * The database table used by the model.
@@ -16,6 +18,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $table = 'users';
 	protected $fillable=array('username','email','password');
+	protected $dates = ['trial_ends_at', 'subscription_ends_at'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
